4dea39df8dd6a0280196b2f6c560dfc3190053bb
Joseph Peck <joe.r.peck@gmail.com>
2023-12-05 13:25:38 -0800

    Recursively adds one or 2 steps. Still too slow though for Leet Code running

--- climbing-stairs/climbing-stairs-ts.ts
+++ climbing-stairs/climbing-stairs-ts.ts
@@ -1,65 +1,38 @@
 function climbStairs(n: number): number {
-    // Max number of double steps is Math.floor(n / 2)
-    // Number of singles is always n
-    // Can always generate an array of singles
-    // Can progress with arrays for iterations and strings for simpleer signature checks
-    let possibilityCount = 0;
-    const maxDoubles = Math.floor(n / 2);
-    const stepOrders: Record<number, string[]> = {
-      0: createSingleStepOrder(n)
-    };
+    const possibilities: string[] = [];
+
+    addStep(possibilities, '', 0, n, false);
 
     if (n > 1) {
-      processDoubles(stepOrders, 1, maxDoubles);
+      addStep(possibilities, '', 0, n, true);
     }
 
-    for (let stepOrder in stepOrders) {
-      possibilityCount += stepOrders[stepOrder].length;
-    }
-
-    return possibilityCount;
+    return possibilities.length;
 };
 
-function createSingleStepOrder(n: number): string[] {
-  let order = '';
+function addStep(possibilities: string[], steps: string, total: number, target: number, addDoubleStep: boolean): void {
+  const newSteps = steps + (addDoubleStep ? '2' : '1');
+  const newTotal = total + (addDoubleStep ? 2 : 1);
 
-  while (order.length < n) {
-    order += '1';
+  if (newTotal === target) {
+    possibilities.push(newSteps);
   }
 
-  return [order];
-}
+  if (newTotal + 1 <= target) {
+    addStep(possibilities, newSteps, newTotal, target, false);
+  }
 
-function processDoubles(stepOrders: Record<number, string[]>, doubleCount: number, maxDoubles: number): void {
-  stepOrders[doubleCount] = [];
-
-  stepOrders[doubleCount - 1].forEach((stepOrder: string) => {
-    for (let firstSingleIndex = 0; firstSingleIndex < stepOrder.length - 1; firstSingleIndex++) {
-      for (let secondSingleIndex = firstSingleIndex + 1; secondSingleIndex < stepOrder.length; secondSingleIndex++) {
-        if (stepOrder[firstSingleIndex] === '1' && stepOrder[secondSingleIndex] === '1') {
-          const potentialAtFirst = stepOrder.slice(0, firstSingleIndex) + '2' + stepOrder.slice(firstSingleIndex + 1, secondSingleIndex) + stepOrder.slice(secondSingleIndex + 1);
-          const potentialAtSecond = stepOrder.slice(0, firstSingleIndex) + stepOrder.slice(firstSingleIndex + 1, secondSingleIndex) + '2' + stepOrder.slice(secondSingleIndex + 1);
-  
-          if (!stepOrders[doubleCount].includes(potentialAtFirst)) {
-            stepOrders[doubleCount].push(potentialAtFirst);
-          }
-  
-          if (!stepOrders[doubleCount].includes(potentialAtSecond)) {
-            stepOrders[doubleCount].push(potentialAtSecond);
-          }
-        }
-      }
-    }
-  });
-
-  if (doubleCount < maxDoubles) {
-    processDoubles(stepOrders, doubleCount + 1, maxDoubles);
+  if (newTotal + 2 <= target) {
+    addStep(possibilities, newSteps, newTotal, target, true);
   }
 }
 
 const climbStairsResult_1 = climbStairs(1);
-console.log(climbStairsResult_1);
+console.assert(climbStairsResult_1 === 1, `Result: ${climbStairsResult_1} | Expected: 1`);
 const climbStairsResult_2 = climbStairs(2);
-console.log(climbStairsResult_2);
+console.assert(climbStairsResult_2 === 2, `Result: ${climbStairsResult_2} | Expected: 2`);
 const climbStairsResult_3 = climbStairs(3);
-console.log(climbStairsResult_3);
+console.assert(climbStairsResult_3 === 3, `Result: ${climbStairsResult_3} | Expected: 3`);
+console.log('1', climbStairsResult_1);
+console.log('2', climbStairsResult_2);
+console.log('3', climbStairsResult_3);
