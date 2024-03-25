fb97055d2ab1b3657a8c5852e3b09096c2bdf5ec
Joseph Peck <joe.r.peck@gmail.com>
2024-03-24 20:17:02 -0700

    Reverse words in string TS

---
+++ maximum-average-subarray-i/max-avg-subarray-i.ts
@@ -0,0 +1,7 @@
+function findMaxAverage(nums: number[], k: number): number {
+  const getAverage = (numbers: number[]): number => {
+    
+  }
+
+  let maxSubAvg = getAverage(nums.slice(0, k));
+};
---
+++ merge-strings-alternately/merge-strings-alternately.ts
@@ -0,0 +1,34 @@
+function mergeAlternately(word1: string, word2: string): string {
+  let output = '';
+  const trustWord1 = word1.length >= word2.length;
+  if (trustWord1) {
+    for (let index = 0; index < word1.length; index++) {
+      output += word1[index];
+      if (word2[index]) {
+        output += word2[index];
+      }
+    }
+
+  } else {
+    for (let index = 0; index < word2.length; index++) {
+      if (word1[index]) {
+        output += word1[index]
+      }
+      output += word2[index];
+    }
+  }
+
+  return output;
+};
+
+console.log('Input', "abc", "pqr");
+console.log('Expected', 'apbqcr');
+console.log('Result', mergeAlternately('abc', 'pqr'));
+console.log('--');
+console.log('Input', "ab", "pqrs");
+console.log('Expected', 'apbqrs');
+console.log('Result', mergeAlternately("ab", "pqrs"));
+console.log('--');
+console.log('Input', "abcd", "pq");
+console.log('Expected', 'apbqcd');
+console.log('Result', mergeAlternately("abcd", "pq"));
